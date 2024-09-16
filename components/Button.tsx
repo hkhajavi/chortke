61a620a9dd86a3d4985f5ce2a4e7a85c09cf504d
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2024-09-16 17:36:31 +0330

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {
   PlasmicButton,
   DefaultButtonProps
-} from "./plasmic/plasmic_tabs/PlasmicButton";
+} from "./plasmic/chortke/PlasmicButton";
 
 import {
   ButtonRef,
