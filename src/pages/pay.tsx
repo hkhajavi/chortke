cbce952987b2a0b1843cf8da37932d596bacd131
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2025-10-07 17:38:58 +0330

    Update pay.tsx

--- src/pages/pay.tsx
+++ src/pages/pay.tsx
@@ -2,9 +2,9 @@
 // This file is owned by you, feel free to edit as you see fit.
 import * as React from "react";
 import { PageParamsProvider as PageParamsProvider__ } from "@plasmicapp/react-web/lib/host";
-import GlobalContextsProvider from "../../../components/plasmic/katibe/PlasmicGlobalContextsProvider";
+import GlobalContextsProvider from "../../components/plasmic/katibe/PlasmicGlobalContextsProvider";
 
-import { PlasmicPay } from "../../../components/plasmic/katibe/PlasmicPay";
+import { PlasmicPay } from "../../components/plasmic/katibe/PlasmicPay";
 import { useRouter } from "next/router";
 
 function Pay() {
